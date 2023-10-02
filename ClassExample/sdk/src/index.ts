37fc6a024296a4ae0a81fe995f17b9b791cd0e25
Karim Machlab <karim@vegascity.org>
2023-10-02 13:55:13 +0100

    info ui refactor

--- ClassExample/sdk/src/index.ts
+++ ClassExample/sdk/src/index.ts
@@ -1,8 +1,6 @@
 
 import { ClassroomManager } from "./classroomManager";
-import { ControllerUI } from "./ui/controllerUI";
 import { setupUI } from "./ui/ui";
 
 setupUI() 
 ClassroomManager.Initialise()
-ControllerUI.Show()
