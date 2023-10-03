54920ec928e36c00341c6050d4284c08962134fd
shaunMcelhinney <shaun@vegascity.org>
2023-10-03 15:38:06 +0100

    TEACH-123 removed from ui

--- ServerExamples/src/ui.tsx
+++ ServerExamples/src/ui.tsx
@@ -1,10 +1,8 @@
 import ReactEcs, { ReactEcsRenderer } from "@dcl/sdk/react-ecs"
-import * as vegascity from "@vegascity/vegas-city-library/index"
 import * as UI from 'dcl-ui-toolkit'
 
 const uiComponent = () => (
   [
-    vegascity.Render(),
     UI.render()
   ]
 )
