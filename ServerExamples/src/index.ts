349c96d2b6af596cf8425650654ecb297510c6bc
shaunMcelhinney <shaun@vegascity.org>
2023-10-03 15:33:19 +0100

    TEACH-123 remove all vc packages

--- ServerExamples/src/index.ts
+++ ServerExamples/src/index.ts
@@ -1,42 +1,33 @@
-import * as vc from "@vegascity/vegas-city-library/index"
-import * as ecs from "@dcl/sdk/ecs"
-import * as sceneData from "../scene.json"
-import { Logger } from "@vegascity/vegas-city-logger/dist/logger/Logger"
 import { ReferenceServerController } from "./messaging/ReferenceServerController"
-import { UserData } from "@vegascity/vegas-city-library/src/core";
-
+import { GetUserDataResponse, getUserData } from '~system/UserIdentity'
 import { Scene } from "./Scene"
 import { setupUi } from "./ui"
 import { VegasCityServerComms } from "./messaging/VegasCityServerComms"
+import { executeTask } from "@dcl/sdk/ecs"
+import { UserData } from "~system/Players"
 
 export function main() {
-  vc.setup({
-    ecs: ecs,
-    Logger: Logger
-  })
+  let userData: GetUserDataResponse | null = null; // Initialize as null
 
-  setupUi()
+  executeTask(async () => {
+    try {
+      userData = await getUserData({});
+      setupUi();
 
-  vc.core.Helper.setSceneBounds(sceneData)
-  
-  vc.core.Helper.init(() => {
-    vc.core.UserData.getUserData((_userData: vc.core.UserData) => {
-      
-      // we can simply change the Url here to inject into the ReferenceServerController to use either the Node implementation or Java
-        let serverUrl = "ws://localhost:3000" //Node
-        //let serverUrl = "ws://localhost:8080/websocket" //Java
+      // We can simply change the Url here to inject into the ReferenceServerController to use either the Node implementation or Java
+       let serverUrl = "ws://localhost:3000" //Node
+      //let serverUrl = "ws://localhost:8080/websocket"; //Java
 
-        // as this scene is not currently connected to a contract we can define different users based on web3 connection to test locally
-        // so a user connected via web3 is a teacher and guests are students
-        let userType = UserData.cachedData.hasConnectedWeb3 ? "teacher" : "student"
+      // Check if userData is not null before accessing its properties
+      let userType = userData?.data?.hasConnectedWeb3 ? "teacher" : "student";
 
-
-        const ws = new ReferenceServerController(UserData.cachedData, userType, serverUrl)
-
-        const serverComms = new VegasCityServerComms(UserData.cachedData)
-
-        new Scene(ws, serverComms)
-
-    })
-  })
+      if (userData) {
+        const ws = new ReferenceServerController(userData.data, userType, serverUrl);
+        const serverComms = new VegasCityServerComms(userData.data);
+        new Scene(ws, serverComms);
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  });
 }
