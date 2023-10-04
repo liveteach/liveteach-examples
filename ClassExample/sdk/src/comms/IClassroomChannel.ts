41b4a2bfdcf7bb148266d975d03f68f5792f2094
Karim Machlab <karim@vegascity.org>
2023-10-04 16:33:25 +0100

    major refactor

--- ClassExample/sdk/src/comms/IClassroomChannel.ts
+++ ClassExample/sdk/src/comms/IClassroomChannel.ts
@@ -1,10 +1,11 @@
-import { StudentCommInfo, TeacherCommInfo } from "../classroom";
+import { ClassPacket, Classroom, StudentCommInfo } from "../classroomObjects";
 
 export interface IClassroomChannel {
-    emitClassActivation(_info: StudentCommInfo);
-    emitClassDeactivation(_info: StudentCommInfo);
-    emitClassStart(_info: StudentCommInfo);
-    emitClassEnd(_info: StudentCommInfo);
-    emitClassJoin(_info: TeacherCommInfo);
-    emitClassExit(_info: TeacherCommInfo);
+    emitClassActivation(_info: ClassPacket);
+    emitClassDeactivation(_info: ClassPacket);
+    emitClassStart(_info: ClassPacket);
+    emitClassEnd(_info: ClassPacket);
+    emitClassJoin(_info: StudentCommInfo);
+    emitClassExit(_info: StudentCommInfo);
+    emitClassroomConfig(_info: Classroom);
 }
