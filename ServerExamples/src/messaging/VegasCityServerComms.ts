e1197a911cf52cea4eb057511ae639270e74329c
shaunMcelhinney <shaun@vegascity.org>
2023-10-04 11:44:18 +0100

    TEACH0123 updates for the internal java implementation

--- ServerExamples/src/messaging/VegasCityServerComms.ts
+++ ServerExamples/src/messaging/VegasCityServerComms.ts
@@ -5,19 +5,25 @@ import { executeTask } from "@dcl/sdk/ecs"
 import { Scene } from "../Scene";
 
 export class VegasCityServerComms {
-    static serverUrl: string = "http://localhost:8080"
+    static serverUrl: string = "http://localhost:8080/"
     private static realm: GetCurrentRealmResponse | null = null
     public static user: UserData
+    private static userType: string
     
     public static instance:VegasCityServerComms
 
-    constructor(user: UserData){
+    constructor(user: UserData, userType: string){
         VegasCityServerComms.serverUrl = VegasCityServerComms.serverUrl
         VegasCityServerComms.instance = this
         VegasCityServerComms.user = user
+        VegasCityServerComms.userType = userType
+        if(userType === "teacher"){
+            this.getGuid()
+        }
+        
     }
 
-    public sendMessage(message: string) {
+    public sendMessage(message: string, topic: string, from: string) {
         let walletAddress: string = VegasCityServerComms.user.publicKey || "GUEST_" + VegasCityServerComms.user.userId
         return executeTask(async ()=>{
             try {
@@ -25,11 +31,13 @@ export class VegasCityServerComms {
                     classId: "b8b3ca99-87d1-4133-90da-529537ef42c9",
                     message:message,
                     user: walletAddress,
-                    guid: Scene.teacherGuid
+                    guid: Scene.teacherGuid,
+                    topic: topic,
+                    from: from
                 }
 
                 let response = await signedFetch({
-                    url: VegasCityServerComms.serverUrl +"api/teacher/command", 
+                    url: VegasCityServerComms.serverUrl +"api/" + VegasCityServerComms.userType +"/command", 
                     init:{
                         headers: { "Content-Type": "application/json" },
                         method: "POST",
@@ -40,9 +48,27 @@ export class VegasCityServerComms {
                 console.log(response)                
 
             } catch (error) {
-                console.log("serverComms.postAnswer() Error "+error)
+                console.log("serverComms.sendMessage() Error "+error)
             }
         })
     }
 
+    public getGuid() {
+        let walletAddress: string = VegasCityServerComms.user.publicKey || "GUEST_" + VegasCityServerComms.user.userId
+        return executeTask(async ()=>{
+            try {
+                let response = await signedFetch({
+                    url: VegasCityServerComms.serverUrl +"api/teacher/guid?message=" + walletAddress, 
+                    init:{
+                        headers: { "Content-Type": "application/json" },
+                        method: "GET"
+                    }
+                    
+                })
+                console.log(response)                
+            } catch (error) {
+                console.log("serverComms.getGuid() Error "+error)
+            }
+        })
+    }
 }
