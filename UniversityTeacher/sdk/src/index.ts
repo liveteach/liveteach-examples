e5c523e42395a43edcec340c550e1cb59b99fe2c
Karim Machlab <karim@vegascity.org>
2023-10-31 15:57:31 +0000

    fixed video sync issue

--- UniversityTeacher/sdk/src/index.ts
+++ UniversityTeacher/sdk/src/index.ts
@@ -8,9 +8,8 @@ import { AudioManager } from "./audioManager";
 import { ControllerUI } from "@dclu/dclu-liveteach/src/classroom/ui/controllerUI";
 import { ClassroomManager } from "@dclu/dclu-liveteach/src/classroom/classroomManager";
 import { PeerToPeerChannel } from "@dclu/dclu-liveteach/src/classroom/comms/peerToPeerChannel";
-import { ScreenDisplay } from "./Screen/screenDisplay";
 import * as classroomConfig from "./classroomConfigs/classroomConfig.json"
-
+import { VideoContent } from "./Screen/content/videoContent";
 
 export function main() {
     new ScreenManager()
@@ -20,9 +19,9 @@ export function main() {
 
     let floor: Entity = engine.addEntity()
     Transform.create(floor, {
-        position: Vector3.create(8,0,8)
+        position: Vector3.create(8, 0, 8)
     })
-    GltfContainer.create(floor, {src: "models/Floor.glb"})
+    GltfContainer.create(floor, { src: "models/Floor.glb" })
 
     setupUi()
     const communicationChannel = new PeerToPeerChannel()
