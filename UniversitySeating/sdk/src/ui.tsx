a7cda3588daf040af5f7008de360397097a13285
Bobby H <bobby@vegascity.org>
2023-11-01 13:12:29 +0000

    Toast call added from the package

--- UniversitySeating/sdk/src/ui.tsx
+++ UniversitySeating/sdk/src/ui.tsx
@@ -2,13 +2,16 @@ import { Color4 } from '@dcl/sdk/math'
 import ReactEcs, { Label, ReactEcsRenderer, UiEntity } from '@dcl/sdk/react-ecs'
 import * as UI from 'dcl-ui-toolkit'
 
+import { Render } from '@dclu/dclu-liveteach'
 import { seatingUIComponent } from '@dclu/dclu-liveteach/src/seating/ui'
+
 
 
 const uiComponent = () => (
   [
     UI.render(),
-    seatingUIComponent()
+    seatingUIComponent(),
+    Render()
   ]
 )
 
