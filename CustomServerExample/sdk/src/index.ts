a831f3f248217c3db039efc1d26c3598e226ac24
Shaun Mcelhinney <shaun@vegascity.org>
2023-11-09 14:25:33 +0000

    TEACH-143 Initial Example scene for the Custom Server Implementation

---
+++ CustomServerExample/sdk/src/index.ts
@@ -0,0 +1,58 @@
+import { GetUserDataResponse, getUserData } from '~system/UserIdentity'
+import { setupUi } from "./ui"
+import { executeTask } from "@dcl/sdk/ecs"
+import { UserData } from "~system/Players"
+import { ClassroomManager } from "@dclu/dclu-liveteach/src/classroom/classroomManager";
+import { CustomServerChannel } from './CustomServerChannel';
+import { ControllerUI } from '@dclu/dclu-liveteach/src/classroom/ui/controllerUI'
+
+import * as classroomConfig from "./classroomConfigs/classroomConfig.json"
+import * as dclu from '@dclu/dclu-liveteach'
+import * as ecs from "@dcl/sdk/ecs"
+import { ServerParams } from '@dclu/dclu-liveteach/src/classroom/types/classroomTypes';
+
+export function main() {
+    let userData: GetUserDataResponse | null = null; // Initialize as null
+
+    // Define the Url for the Webscoket Server
+    let serverUrl = "ws://localhost:3000"
+
+    // Setup the DCLU package
+    dclu.setup({
+      ecs: ecs,
+      Logger: null
+    })
+
+    //setup the User Interface
+    setupUi()
+
+    executeTask(async () => {
+
+      try {
+
+        userData = await getUserData({});
+          
+          //Is the user the Teacher
+        let userType = userData?.data?.publicKey === classroomConfig.classroom.teacherID ? "teacher" : "student";
+        
+        //setup Server Parameters for the Websocket Server
+        let params: ServerParams = {
+          serverUrl: serverUrl,
+          role: userType,
+          _userData: userData
+        }
+
+        //Define the Channel to be used Using the Custom ServerChannel Class
+        const communicationChannel = new CustomServerChannel();
+        //Pass in the Server Parameters
+        communicationChannel.serverConfig(params)
+        // Initialise the Classroom Manager
+        ClassroomManager.Initialise(classroomConfig, communicationChannel)   
+        //show Control ui for teacher         
+        ControllerUI.Show()
+
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+      }
+    });
+  }
---
+++ CustomServerExample/sdk/src/ui.tsx
@@ -0,0 +1,12 @@
+import ReactEcs, { ReactEcsRenderer } from "@dcl/sdk/react-ecs"
+import { Render } from '@dclu/dclu-liveteach/src/setup/ui'
+
+const uiComponent = () => (
+  [
+    Render()
+  ]
+)
+
+export function setupUi() {
+  ReactEcsRenderer.setUiRenderer(uiComponent)
+}
