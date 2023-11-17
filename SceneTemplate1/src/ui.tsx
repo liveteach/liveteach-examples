e6941e2212e8b90ece576e8552f4a762181cf311
Karim Machlab <karim@vegascity.org>
2023-11-17 12:01:26 +0000

    poll content unit

--- SceneTemplate1/src/ui.tsx
+++ SceneTemplate1/src/ui.tsx
@@ -1,11 +1,13 @@
 import { ReactEcsRenderer } from "@dcl/sdk/react-ecs"
 import { seatingUIComponent } from "@dclu/dclu-liveteach/src/seating/ui"
 import { Render } from '@dclu/dclu-liveteach/src/setup/ui'
+import  *  as  ui  from  'dcl-ui-toolkit'
 
 const uiComponent = () => (
     [
       //seatingUIComponent(), // Hide seat UI for now
-      Render()
+      Render(),
+      ui.render()
     ]
   )
 
