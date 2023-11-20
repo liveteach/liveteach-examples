4913c732ceb4c4171dc0468be1005c9f4b4a3878
Karim Machlab <karim@vegascity.org>
2023-11-20 15:11:11 +0000

    adjusted authentication scene

--- TeacherAuthenticationExample/src/index.ts
+++ TeacherAuthenticationExample/src/index.ts
@@ -1,83 +1,82 @@
 // We define the empty imports so the auto-complete feature works as expected.
-import { } from '@dcl/sdk/math'
-import { Entity, InputAction, MeshCollider, MeshRenderer, Transform, engine, executeTask, pointerEventsSystem } from '@dcl/sdk/ecs'
-import { getUserData } from "~system/UserIdentity"
-import { RequestManager, ContractFactory } from "eth-connect"
-import { createEthereumProvider } from '@dcl/sdk/ethereum-provider'
-import abi from "./TeachContractAbi.json"
-import { GetSceneResponse, getSceneInfo } from '~system/Scene'
+import { Color4 } from '@dcl/sdk/math'
+import { Entity, Material, MeshCollider, MeshRenderer, Transform, engine, executeTask } from '@dcl/sdk/ecs'
+import { PeerToPeerChannel } from "@dclu/dclu-liveteach/src/classroom/comms/peerToPeerChannel";
+import { ClassroomManager } from "@dclu/dclu-liveteach/src/classroom"
+import { GetSceneResponse, getSceneInfo } from '~system/Scene';
+import * as ecs from "@dcl/sdk/ecs"
+import * as dclu from '@dclu/dclu-liveteach'
+import * as classroomConfig from "./classroomConfigs/classroomConfig.json"
+
+const cubes: Entity[] = []
+var sceneBaseX: number = 0
+var sceneBaseZ: number = 0
 
 export function main() {
-  createCube("Authenticate -55,0 ", 8, 1, 8);
-  createCube("Authenticate -55,1 ", 8, 1, 24);
+  getSceneData()
+
+  dclu.setup({
+    ecs: ecs,
+    Logger: null
+  })
+
+  const communicationChannel = new PeerToPeerChannel()
+  ClassroomManager.Initialise(classroomConfig, communicationChannel)
+  createCube(8, 1, 8)
+  createCube(8, 1, 24)
+
+  engine.addSystem(update)
 }
 
-async function getEntityParcel(entity: Entity): Promise<[number, number]> {
-  let sceneInfo: GetSceneResponse = await getSceneInfo({});
-  if (!sceneInfo || !sceneInfo.metadata) {
-    throw new Error('Cannot get scene info');
-  }
-  let parsedMetaData: any = JSON.parse(sceneInfo.metadata)
-  if (!parsedMetaData || !parsedMetaData.scene || !parsedMetaData.scene.base) {
-    throw new Error('Cannot get scene info from parsed metadata');
-  }
-  let parcel: string = parsedMetaData.scene.base
-  let sceneBaseArr = parcel.split(",")
-  let sceneBaseX = parseInt(sceneBaseArr[0])
-  let sceneBaseZ = parseInt(sceneBaseArr[1])
+function createCube(x: number, y: number, z: number): void {
+  const meshEntity = engine.addEntity()
+  Transform.create(meshEntity, { position: { x, y, z } })
+  MeshRenderer.setBox(meshEntity)
+  MeshCollider.setBox(meshEntity)
+  Material.setPbrMaterial(meshEntity, {
+    albedoColor: Color4.Red()
+  })
 
-  let worldPositionX = sceneBaseX + (Transform.get(entity).position.x / 16)
-  let worldPositionZ = sceneBaseZ + (Transform.get(entity).position.z / 16)
+  cubes.push(meshEntity)
+}
+
+function update(): void {
+  const authenticated: boolean = ClassroomManager.classController?.isTeacher()
+  const userParcel = getEntityParcel(engine.PlayerEntity)
+  console.log(authenticated)
+
+  cubes.forEach(cube => {
+    const cubeParcel = getEntityParcel(cube)
+    if (cubeParcel[0] == userParcel[0] && cubeParcel[1] == userParcel[1]) {
+      Material.setPbrMaterial(cube, {
+        albedoColor: authenticated ? Color4.Green() : Color4.Red()
+      })
+    }
+  });
+}
+
+function getEntityParcel(_entity: Entity): [number, number] {
+  const pos = Transform.get(_entity).position
+  let worldPositionX = sceneBaseX + (pos.x / 16)
+  let worldPositionZ = sceneBaseZ + (pos.z / 16)
   worldPositionX = Math.floor(worldPositionX)
   worldPositionZ = Math.floor(worldPositionZ)
   return [worldPositionX, worldPositionZ]
 }
 
-function createCube(text: string, x: number, y: number, z: number): Entity {
-  const meshEntity = engine.addEntity()
-  Transform.create(meshEntity, { position: { x, y, z } })
-  MeshRenderer.setBox(meshEntity)
-  MeshCollider.setBox(meshEntity)
-
-  pointerEventsSystem.onPointerDown(
-    {
-      entity: meshEntity,
-      opts: { button: InputAction.IA_POINTER, hoverText: text },
-    },
-    function () {
-      executeTask(async () => {
-        let [parcelX, parcelY]: [number, number] = await getEntityParcel(meshEntity)
-        // let classroomData:void | ClassPacket[] = await ClassroomManager.ActivateClassroom(parcelX, parcelY);
-        // console.log(classroomData)
-        let userData = await getUserData({})
-        if (userData && userData.data && userData.data.hasConnectedWeb3) {
-          console.log("wallet address", userData.data.publicKey)
-          // create an instance of the web3 provider to interface with Metamask
-          const provider = createEthereumProvider()
-          // Create the object that will handle the sending and receiving of RPC messages
-          const requestManager = new RequestManager(provider)
-          // Create a factory object based on the abi
-          const factory = new ContractFactory(requestManager, abi)
-          // Use the factory object to instance a `contract` object, referencing a specific contract
-          const contract = (await factory.at(
-            "0x3185cafec6fc18267ac92f83ffc8f08658519097"
-          )) as any
-
-          const res = await contract.getClassroomGuid(
-            parcelX, parcelY,
-            {
-              from: userData.data.publicKey,
-            }
-          )
-          // Log response
-          console.log("Classroom Guid", res)
-
-        } else {
-          console.log("Player is not connected with Web3")
-        }
-      })
+function getSceneData() {
+  executeTask(async () => {
+    let sceneInfo: GetSceneResponse = await getSceneInfo({});
+    if (!sceneInfo || !sceneInfo.metadata) {
+      throw new Error('Cannot get scene info');
     }
-  )
-
-  return meshEntity
+    let parsedMetaData: any = JSON.parse(sceneInfo.metadata)
+    if (!parsedMetaData || !parsedMetaData.scene || !parsedMetaData.scene.base) {
+      throw new Error('Cannot get scene info from parsed metadata');
+    }
+    let parcel: string = parsedMetaData.scene.base
+    let sceneBaseArr = parcel.split(",")
+    sceneBaseX = parseInt(sceneBaseArr[0])
+    sceneBaseZ = parseInt(sceneBaseArr[1])
+  })
 }
